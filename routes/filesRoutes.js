c554a9770e1c7cdfd77e9c1becba496daf35e10f
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-03-12 21:33:00 -0600

    se implemento el midleyware para guardar el archivo

--- routes/filesRoutes.js
+++ routes/filesRoutes.js
@@ -1,4 +1,31 @@
-import { Router } from "express";
+import express, { Router } from "express";
+import multer from "multer";
+import { saveFile } from "../controllers/files.js";
+import { extname } from 'path'
 
 export const filesRoutes=Router();
 
+const tipos=["image/jpeg","image/png"]
+
+const multerUpload=multer({
+    storage: multer.diskStorage({
+            //destination: join(currentDir, '../uploads'),
+            destination: './uploads',
+            filename: (req,file,cb)=>{
+                const fielExtension = extname(file.originalname)
+                const fileName = file.originalname.split(fielExtension)[0];
+                cb(null, `${fileName}-${Date.now()}${fielExtension}`)
+            }
+        }),
+        fileFilter: (req,file,cb)=>{
+                if(tipos.includes(file.mimetype)) cb(null,true)
+                else cb(new Error('Only image/jpeg or image/png are are allowed'))
+        },
+        limits: {
+                fieldSize: 200000000
+        }
+});
+
+filesRoutes.post('/dropfile',multerUpload.single('file'),saveFile);
+
+filesRoutes.get('/getfile',express.static('./uploads'));
